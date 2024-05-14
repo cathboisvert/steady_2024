33f5c8c4be0edf06596b3d8e632dd46c81f891bd
cathboisvert <catherine.boisvert4@mail.mcgill.ca>
2024-05-14 13:47:03 -0400

    Adding another function in math calculations.

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-from stready_calculations.math_operations import add
+from stready_calculations.math_operations import add, div
 
 x=13.4
 y=23.4
 
 print("Hello, world!")
 print(add(x,y))
-
-print("Bonjour")
+print(div(x/y))
